20693752a7b12a6d5c9529ba29d815480e549f0c
Jayden Grubb <jaydengrubb1@gmail.com>
2024-05-06 15:54:45 +1000

    recursively check for dependencies (ROS only)

--- wisteria/main.py
+++ wisteria/main.py
@@ -63,26 +63,61 @@ def main():
 
     walker = DependencyWalker(distro, os.environ)
 
-    dependencies = walker.get_recursive_depends(
-        pkg_name=args.package,
-        depend_types=args.types,
-        ros_packages_only=False,
-        ignore_pkgs=None,
-        limit_depth=1,
-    )
+    # dependencies = walker.get_recursive_depends(
+    #     pkg_name=args.package,
+    #     depend_types=args.types,
+    #     ros_packages_only=False,
+    #     ignore_pkgs=None,
+    #     limit_depth=1,
+    # )
 
-    for dep in dependencies:
-        version = "unknown"
-        try:
-            pkg = distro.release_packages[dep]
-            repo = distro.repositories[pkg.repository_name].release_repository
-            # tag = get_release_tag(repo, dep)
-            # tag is in the form of "release/{distro}/{pkg}/{version}"
-            # IDK if tag is needed for anything
-            version = repo.version
-        except Exception as e:
-            pass
-        print("{0} ({1})".format(dep, version))
+    # for dep in dependencies:
+    #     version = "unknown"
+    #     try:
+    #         pkg = distro.release_packages[dep]
+    #         repo = distro.repositories[pkg.repository_name].release_repository
+    #         # tag = get_release_tag(repo, dep)
+    #         # tag is in the form of "release/{distro}/{pkg}/{version}"
+    #         # IDK if tag is needed for anything
+    #         version = repo.version
+    #     except Exception as e:
+    #         pass
+    #     print("{0} ({1})".format(dep, version))
+
+    visited = {}
+
+    def walk_dependencies(name):
+        if name in visited:
+            return visited[name]
+
+        if not name in distro.release_packages:
+            # TODO check conda index
+            print("\033[93m{0}\033[0m".format(name))
+            visited[name] = True
+            return True
+
+        dependencies = walker.get_recursive_depends(
+            pkg_name=name,
+            depend_types=args.types,
+            ros_packages_only=False,
+            ignore_pkgs=None,
+            limit_depth=1,
+        )
+
+        for dep in dependencies:
+            if not walk_dependencies(dep):
+                return False
+
+        # process ROS package
+        pkg = distro.release_packages[name]
+        repo = distro.repositories[pkg.repository_name].release_repository
+        version = repo.version
+        print("{0} ({1})".format(name, version))
+
+        visited[name] = True
+        return True
+
+    walk_dependencies(args.package)
 
 
 if __name__ == "__main__":
